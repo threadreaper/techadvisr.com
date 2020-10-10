67873da6a6d373df4084d7179646270c431bbe9a
Michael <threadreaper@gmail.com>
2020-10-10 00:55:08 -0500

    modal popup functionality added to flux

--- js/flux.js
+++ js/flux.js
@@ -11,6 +11,89 @@ function classToggle() {
     }
 }
 
+var modal, modalButton, modalClose;
+var modal = document.querySelector('.modal');
+
+function showModal() {
+    storage = window.sessionStorage;
+    
+  if (storage.getItem('modalSeen'))
+        return;
+    storage.setItem('modalSeen','true');
+    modal.style.display = 'block';
+}
+
+modalReveal = document.querySelector('.modal-reveal');
+modalReveal.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+document.addEventListener("click", (evt) => {
+    let targetElement = evt.target; // clicked element
+    if (targetElement != modalReveal) {
+        do {
+            if (targetElement == modal) {
+                    return;
+            }
+                targetElement = targetElement.parentNode;
+        } while (targetElement);
+                modal.style.display = 'none';
+    }
+});
+
+modalClose = document.querySelector('.modal-close');
+modalClose.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+document.onscroll = function(){ 
+  var pos = getVerticalScrollPercentage(document.body)
+  var modalTrigger = getComputedStyle(document.documentElement).getPropertyValue('--modal-trigger').trim();
+    if (modalTrigger) {
+      if (pos > modalTrigger) {
+          showModal();
+      }
+    }
+};
+
+function getVerticalScrollPercentage( elm ){
+    var p = elm.parentNode,
+        pos = (elm.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight ) * 100
+    
+    return pos
+}
+
+
+document.addEventListener('mouseleave', function(event) {    
+    var exitIntent = getComputedStyle(document.documentElement).getPropertyValue('--exit-intent').trim();
+
+    // If this is an autocomplete element.
+    if(event.target.tagName)
+        if(event.target.tagName.toLowerCase() == "input")
+            return;
+
+    // Get the current viewport width.
+    var vpWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+    // If the current mouse X position is within 50px of the right edge
+    // of the viewport, return.
+    if(event.clientX >= (vpWidth - 50))
+        return;
+
+    // If the current mouse Y position is not within 50px of the top
+    // edge of the viewport, return.
+    if(event.clientY >= 50)
+        return;
+
+    // Reliable, works on mouse exiting window and
+    // user switching active program
+    var from = event.relatedTarget || event.toElement;
+    if(!from)
+        if(exitIntent == 1)
+            showModal();
+});
+
+
 var icons;
 icons = document.querySelectorAll('.icon');
 
@@ -378,4 +461,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
             classToggle();
         });
     }
+    
 });
